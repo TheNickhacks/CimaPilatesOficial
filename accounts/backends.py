f1467b2b290c4403d507d03c2aec9d8df6401188
TheNickhacks <nnavarrog2@uft.edu>
2026-07-08 19:30:07 -0400

    Optimizacion de carga de foto de perfil, alto consumo de datos en la bbdd NN0807

--- accounts/backends.py
+++ accounts/backends.py
@@ -7,13 +7,18 @@ class EmailBackend(ModelBackend):
         email = (kwargs.get("email") or username or "").strip().lower()
         if not email or not password:
             return None
-
         UserModel = get_user_model()
         try:
-            user = UserModel.objects.get(email__iexact=email)
+            user = UserModel.objects.defer("foto_perfil_data").get(email__iexact=email)
         except UserModel.DoesNotExist:
             return None
-
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
         return None
+
+    def get_user(self, user_id):
+        UserModel = get_user_model()
+        try:
+            return UserModel.objects.defer("foto_perfil_data").get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return None
